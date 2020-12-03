929a7c1f511376015e3df74a4135302679605dcb
Tajmeet Singh <tjgurwara99@gmail.com>
2020-12-03 22:11:26 +0000

    fix: rearranged model functions to separate concerns. CompanyModel for company stuff and UserModel for user stuff.

--- model/company_model.go
+++ model/company_model.go
@@ -69,3 +69,94 @@ func (companyModel *CompanyModel) GetCompanyByName(companyName string) (*entity.
 	}
 	return &company, nil
 }
+
+// GetEmployees active users of a company for CompanyModel - restrict it to per company
+func (companyModel *CompanyModel) GetEmployees(userCompany *entity.Company) (*[]entity.User, error) {
+
+	rows, err := companyModel.DB.Query(
+		`select id, password, first_name, last_name, last_login, is_superuser, username,
+		email, is_staff, is_active, date_joined, company_id, birth_date 
+		from user where (company_id = ?) and (is_active=true)`, userCompany.CompanyID)
+	if err != nil {
+		return nil, err
+	}
+
+	var users []entity.User
+
+	for rows.Next() {
+		var user entity.User
+		err := rows.Scan(&user.FirstName, &user.LastName, &user.Email)
+
+		if err != nil {
+			return nil, err
+		}
+
+		users = append(users, user)
+	}
+	return &users, nil
+}
+
+// GetCompanyManagers Get company managers returns a pointer to a slice of entity.User
+func (companyModel *CompanyModel) GetCompanyManagers(company *entity.Company) (*[]entity.User, error) {
+	rows, err := companyModel.DB.Query(`
+	select id, password, first_name, last_name, last_login, is_superuser, username,
+	email, is_staff, is_active, date_joined, birth_date, is_owner
+	from user where (company_id = ?) and (is_manager = true)`, company.CompanyID)
+
+	if err != nil {
+		return nil, err
+	}
+
+	var managers []entity.User
+
+	for rows.Next() {
+		var manager entity.User
+		manager.IsManager = true
+		err := rows.Scan(&manager.UserID, &manager.Password,
+			&manager.FirstName, &manager.LastName,
+			&manager.LastLogin, &manager.IsSuperuser,
+			&manager.Username, &manager.Email,
+			&manager.IsStaff, &manager.IsActive,
+			&manager.DateJoined, &manager.BirthDate, &manager.IsOwner)
+		if err != nil {
+			return nil, err
+		}
+		manager.Company = company
+		managers = append(managers, manager)
+	}
+
+	return &managers, nil
+
+}
+
+// GetCompanyOwners Get Company Owners from the database
+func (companyModel *CompanyModel) GetCompanyOwners(company *entity.Company) (*[]entity.User, error) {
+	rows, err := companyModel.DB.Query(`
+	select id, password, first_name, last_name, last_login, is_superuser, username,
+	email, is_staff, is_active, date_joined, birth_date, is_manager
+	from user where (company_id = ?) and (is_owner = true)`, company.CompanyID)
+
+	if err != nil {
+		return nil, err
+	}
+
+	var owners []entity.User
+
+	for rows.Next() {
+		var owner entity.User
+		owner.IsOwner = true
+		err := rows.Scan(&owner.UserID, &owner.Password,
+			&owner.FirstName, &owner.LastName,
+			&owner.LastLogin, &owner.IsSuperuser,
+			&owner.Username, &owner.Email,
+			&owner.IsStaff, &owner.IsActive,
+			&owner.DateJoined, &owner.BirthDate,
+			&owner.IsManager)
+		if err != nil {
+			return nil, err
+		}
+		owner.Company = company
+		owners = append(owners, owner)
+	}
+	return &owners, nil
+}
--- model/user_model.go
+++ model/user_model.go
@@ -27,28 +27,6 @@ func (userModel *UserModel) CreateUser(newUser *entity.User) (*entity.User, erro
 
 }
 
-// GetCompanyUsers active users of a company for UserModel - restrict it to per company
-func (userModel *UserModel) GetCompanyUsers(userCompany entity.Company) (*[]entity.User, error) {
-	rows, err := userModel.DB.Query("select first_name, last_name, email from user where (company_id = ?) and (is_active=true) ", userCompany.CompanyID)
-	if err != nil {
-		return nil, err
-	}
-
-	var users []entity.User
-
-	for rows.Next() {
-		var user entity.User
-		err := rows.Scan(&user.FirstName, &user.LastName, &user.Email)
-
-		if err != nil {
-			return nil, err
-		}
-
-		users = append(users, user)
-	}
-	return &users, nil
-}
-
 // GetUserByID Get User by UserID
 func (userModel *UserModel) GetUserByID(userID int64) (*entity.User, error) {
 	rows, err := userModel.DB.Query(`
@@ -87,71 +65,6 @@ func (userModel *UserModel) GetUserByID(userID int64) (*entity.User, error) {
 	}
 	return &user, nil
 
-}
-
-// GetCompanyManagers Get company managers returns a pointer to a slice of entity.User
-func (userModel *UserModel) GetCompanyManagers(company *entity.Company) (*[]entity.User, error) {
-	rows, err := userModel.DB.Query(`
-	select id, password, first_name, last_name, last_login, is_superuser, username,
-	email, is_staff, is_active, date_joined, birth_date, is_owner
-	from user where (company_id = ?) and (is_manager = true)`, company.CompanyID)
-
-	if err != nil {
-		return nil, err
-	}
-
-	var managers []entity.User
-
-	for rows.Next() {
-		var manager entity.User
-		manager.IsManager = true
-		err := rows.Scan(&manager.UserID, &manager.Password,
-			&manager.FirstName, &manager.LastName,
-			&manager.LastLogin, &manager.IsSuperuser,
-			&manager.Username, &manager.Email,
-			&manager.IsStaff, &manager.IsActive,
-			&manager.DateJoined, &manager.BirthDate, &manager.IsOwner)
-		if err != nil {
-			return nil, err
-		}
-		manager.Company = company
-		managers = append(managers, manager)
-	}
-
-	return &managers, nil
-
-}
-
-// GetCompanyOwners Get Company Owners from the database
-func (userModel *UserModel) GetCompanyOwners(company *entity.Company) (*[]entity.User, error) {
-	rows, err := userModel.DB.Query(`
-	select id, password, first_name, last_name, last_login, is_superuser, username,
-	email, is_staff, is_active, date_joined, birth_date, is_manager
-	from user where (company_id = ?) and (is_owner = true)`, company.CompanyID)
-
-	if err != nil {
-		return nil, err
-	}
-
-	var owners []entity.User
-
-	for rows.Next() {
-		var owner entity.User
-		owner.IsOwner = true
-		err := rows.Scan(&owner.UserID, &owner.Password,
-			&owner.FirstName, &owner.LastName,
-			&owner.LastLogin, &owner.IsSuperuser,
-			&owner.Username, &owner.Email,
-			&owner.IsStaff, &owner.IsActive,
-			&owner.DateJoined, &owner.BirthDate,
-			&owner.IsManager)
-		if err != nil {
-			return nil, err
-		}
-		owner.Company = company
-		owners = append(owners, owner)
-	}
-	return &owners, nil
 }
 
 //GetUserByUsername Get User by Username
