f2d6475651d5a68d9d5e6922df360f8806ed0d95
Tajmeet Singh <tjgurwara99@gmail.com>
2021-02-27 01:27:33 +0000

    fix: some minor changes

--- config/config.go
+++ config/config.go
@@ -4,7 +4,7 @@ import "os"
 
 // GetConfs returns configurations strings for database connection
 func GetConfs() (string, string, string, string, string) {
-	switch statement := os.Getenv("environemtn"); statement {
+	switch statement := os.Getenv("environment"); statement {
 	case "production":
 		return "mysql", os.Getenv("DATABASE_USER"), os.Getenv("DATABASE_PASSWORD"), "127.0.0.1", os.Getenv("DATABASE_NAME")
 	default:
--- entity/company.go
+++ entity/company.go
@@ -4,10 +4,11 @@ import "time"
 
 // Company Entity
 type Company struct {
-	CompanyID       int64
-	CompanyName     string
-	CompanyIsActive bool
-	LastPayment     time.Time
+	CompanyID       uint64    `json:"company_id" db:"compant_id"`
+	CompanyName     string    `json:"company_name" db:"company_name"`
+	CompanyIsActive bool      `json:"company_is_active" db:"is_active"`
+	LastPayment     time.Time `json:"last_payment" db:"last_payment"`
+	MainPosition    string    `json:"main_position" db:"main_position"`
 }
 
 // String function to return default value for fmt.Printf like functions
